315512bf04af9f42a54f2625da9bcc98b7182c76
mustahafam <mohamed.musthafa2@cognizant.com>
2017-05-16 10:31:47 +0530

    ChromeChanges

--- SeleniumProject/src/main/java/com/practise/selenium/framework/BaseClass.java
+++ SeleniumProject/src/main/java/com/practise/selenium/framework/BaseClass.java
@@ -6,17 +6,23 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	protected WebDriver driver;
 	
+	String Chromprop = "webdriver.chrome.driver";
+	String ChromeDriverPath=".\\src\\main\\resources\\ChromeDriver\\chromedriver.exe";
 	
 	@BeforeMethod
-	public void setupDriver(){
-		System.setProperty("webdriver.chrome.driver", "D:\\javaprac\\Practise\\selenium\\SeleniumFW\\SeleniumProject\\src\\main\\resources\\ChromeDriver\\chromedriver.exe");
+	@Parameters("browser")
+	public void setupChromeDriver(String browser){
+		if(browser.equalsIgnoreCase("chrome")){
+		System.setProperty(Chromprop, ChromeDriverPath);
 		driver = new ChromeDriver();
-		
+		}
 	}
+	
 	@BeforeTest
 	public WebDriver getDriver() {
 		return driver;
--- SeleniumProject/src/test/java/myAuto/practice/selenium/TC1.java
+++ SeleniumProject/src/test/java/myAuto/practice/selenium/TC1.java
@@ -1,6 +1,6 @@
 package myAuto.practice.selenium;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -8,18 +8,19 @@ import org.testng.annotations.Test;
 import com.practise.selenium.framework.BaseClass;
 
 public class TC1 extends BaseClass{
-	/*@BeforeTest
+	/*WebDriver driver;
+	@BeforeTest
 	public void BrowserInitite()
 	{
-		System.setProperty("webdriver.chrome.driver", "D:\\javaprac\\Practise\\selenium\\SeleniumFW\\SeleniumProject\\src\\main\\resources\\ChromeDriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Me\\SeleniumProject\\src\\main\\resources\\ChromeDriver\\chromedriver.exe");
 		driver = new ChromeDriver();
 		
 	}*/
 	@Test
-	public void init() throws InterruptedException
+	public void FirstTestCase() throws InterruptedException
 	{
 	    System.out.print("++++++++++++++++++++Start++++++++++++++++++++++++");
 	    Thread.sleep(5000);
-		driver.get("https://www.facebook.com/");	
+		driver.get("https://www.Gmail.com/");	
 	}
 }
