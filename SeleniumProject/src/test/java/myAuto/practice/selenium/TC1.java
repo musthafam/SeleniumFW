7f24a9b00d27ab781bbb44b53e6905c871611d15
musthafam <mmmgce@gmail.com>
2017-05-21 15:22:12 +0530

    ExcelUtility

--- SeleniumProject/src/test/java/myAuto/practice/selenium/TC1.java
+++ SeleniumProject/src/test/java/myAuto/practice/selenium/TC1.java
@@ -2,6 +2,7 @@ package myAuto.practice.selenium;
 
 import org.testng.annotations.Test;
 import com.practise.selenium.framework.BaseClass;
+import com.practise.selenium.framework.Excelutility;
 
 public class TC1 extends BaseClass{
 	/*WebDriver driver;
@@ -18,5 +19,10 @@ public class TC1 extends BaseClass{
 	    System.out.print("++++++++++++++++++++Start++++++++++++++++++++++++");
 	    Thread.sleep(5000);
 		driver.get("https://www.twitter.com/");	
+		System.out.print("++++++++++++++++++++Excel++++++++++++++++++++++++");
+	    String output = Excelutility.getCellData(1, 1);
+	    System.out.print(output);	
 	}
+	
+	
 }
