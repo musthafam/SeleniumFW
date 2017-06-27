fa310ed2e6e7b034a69b75b80b0ef3a72262b0b8
musthafam <mmmgce@gmail.com>
2017-06-27 18:21:14 +0530

    init

--- SeleniumProject/src/test/java/myAuto/practice/selenium/apitest.java
+++ SeleniumProject/src/test/java/myAuto/practice/selenium/apitest.java
@@ -2,11 +2,15 @@ package myAuto.practice.selenium;
 
 import org.testng.annotations.Test;
 
+import com.jayway.restassured.response.Response;
+
 public class apitest {
 
 	@Test
 	public void ApiTest1()
 	{
-		
+		Response response = com.jayway.restassured.RestAssured.get("https://api.github.com/users");
+	    String json = response.asString();
+	    System.out.print(json);
 	}
 }
